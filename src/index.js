36421ceeed6a499a9b1116b24e234eac45c7159f
Mohd Shonez <140795032+mayonez-here@users.noreply.github.com>
2025-08-13 02:08:20 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import ErrorBoundary from './ErrorBoundary';
+ReactDOM.render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+  document.getElementById('root')
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
